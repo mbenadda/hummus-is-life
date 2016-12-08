9519c8a44eb10c710044cede4abd384f17a94cdf
Mehdi Benadda <me@mbenadda.com>
2016-12-08 15:46:30 +0100

    wip

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict';
+var taskMgr = angular.module('taskMgr', ['todoList']);
+var todoList = angular.module('todoList',[]);
+
+todoList.controller('todoCtrl', ['$scope', function ($scope) {
+    var todos = $scope.todos = [];
+    $scope.addTodo = function () {
+        var newTodo = $scope.newTodo.trim();
+        if (!newTodo.length) {
+            return;
+        }
+        todos.push({
+            title: newTodo,
+            completed: false
+        });
+        $scope.newTodo = '';
+    };
+    $scope.removeTodo = function (todo) {
+        todos.splice(todos.indexOf(todo), 1);
+    };
+    $scope.markAll = function (completed) {
+        todos.forEach(function (todo) {
+            todo.completed = !completed;
+        });
+    };
+    $scope.clearCompletedTodos = function () {
+      $scope.todos = todos = todos.filter(function (todo) {
+        return !todo.completed;
+      });
+    };
+}]);
